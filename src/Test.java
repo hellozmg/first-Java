53ac5f112a1b2ff666cab4444b4b457a2f27007e
hellozmg <791738673@qq.com>
2014-05-29 19:15:41 +0800

    This is my first Java project！

--- src/Test.java
+++ src/Test.java
@@ -12,19 +12,29 @@ public class Test {
 
 	
 	zs.eat("鸡蛋");
-	zs.sleep("大床");
-	
 	ls.eat("青菜");
+	
+	zs.sleep("大床");
 	ls.sleep("铁床");
-
+	
+	zs.drive("宝马");
+	ls.drive("奔驰");
+	
+	
 	
 	}
 	 void eat(String food) {
 	System.out.println(name+"   正在吃    "+food+"!");
+	
 		
 	}
 	 void sleep(String chuang){
 		 System.out.println(name+" 正在睡    "+chuang+"!");
+	 }
+	 
+	 void drive(String che){
+		 
+		 System.out.println(name+"在开"+che);
 	 }
  
 
